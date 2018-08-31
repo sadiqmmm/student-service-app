0edd5ce8db233d1ffcca82ad519c034c1965788f
jordanhudgens <jordan@devcamp.com>
2018-08-31 10:52:16 -0500

    Built out server file

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
+const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
-})
+app.enable("trust proxy");
+
+app.use(function(req, res, next) {
+  console.log("inside app use");
+
+  if (req.protocol !== "https") {
+    console.log("inside conditional");
+    var secureUrl = "https://" + req.headers["host"] + req.url;
+    res.writeHead(301, { Location: secureUrl });
+    res.end();
+  }
+  next();
+});
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  console.log("inside get");
+  res.sendFile(__dirname + "/dist/index.html");
+});
 app.listen(port);
 
 console.log("server started");
