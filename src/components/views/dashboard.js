a4f80934e13a43fbfe5da4268b05717491e659ec
jordanhudgens <jordan@devcamp.com>
2018-09-15 15:14:02 -0700

    Added query to the project list from the dashboard

--- src/components/views/dashboard.js
+++ src/components/views/dashboard.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Dashboard extends Component {
+  getProjectList() {
+    axios
+      .get("https://api.devcamp.space/projects", {
+        withCredentials: true
+      })
+      .then(response => {
+        console.log("client project", response);
+      })
+      .catch(error => {
+        console.log("Errors");
+      });
+  }
+
+  componentDidMount() {
+    this.getProjectList();
+  }
   render() {
     return (
       <div>
