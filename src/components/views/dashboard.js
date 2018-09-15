5bd8fe34d8adee714413a300952deff97204d5d7
jordanhudgens <jordan@devcamp.com>
2018-09-15 15:28:36 -0700

    Built out process for calling projects and rendering them on the page

--- src/components/views/dashboard.js
+++ src/components/views/dashboard.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class Dashboard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      projects: []
+    };
+  }
+
   getProjectList() {
     axios
       .get("https://api.devcamp.space/projects", {
         withCredentials: true
       })
       .then(response => {
+        this.setState({
+          projects: response.data.projects
+        });
+
         console.log("client project", response);
       })
       .catch(error => {
@@ -18,12 +30,19 @@ export default class Dashboard extends Component {
   componentDidMount() {
     this.getProjectList();
   }
+
   render() {
+    const projects = this.state.projects.map(project => {
+      return <div key={project.id}>{project.title}</div>;
+    });
+
     return (
       <div>
         <h1>Dashboard</h1>
 
         <p>And a bunch of data goes here</p>
+
+        {projects}
       </div>
     );
   }
