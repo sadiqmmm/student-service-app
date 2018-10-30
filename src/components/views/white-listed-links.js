3ec6829f8e49ee79825830c4403a7c001f27f10a
jordanhudgens <jordan@devcamp.com>
2018-10-30 16:03:41 -0500

    Added the white listed link component and set up the initial state items

--- src/components/views/white-listed-links.js
+++ src/components/views/white-listed-links.js
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 import DashboardNavigation from "../partials/navigation";
 
 export default class WhiteListedLinks extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      clientDomains: []
+    };
+  }
+
+  componentDidMount() {
+    this.getWhiteListLinks();
+  }
+
+  getWhiteListLinks() {
+    // TODO Get the whtie list links from API, doesn't need subdomain
+    console.log("Getting white list links");
+  }
+
   render() {
     return (
       <div>
