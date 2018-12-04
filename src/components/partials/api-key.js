fc2be84d19958c745a72a02fe57a40a77526be83
jordanhudgens <jordan@devcamp.com>
2018-12-03 18:19:47 -0700

    Refactored api key component to get the api key when mounted

--- src/components/partials/api-key.js
+++ src/components/partials/api-key.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function() {
-  return (
-    <div className="card">
-      <h2>API Key</h2>
-      <div className="two-column-grid">
-        <div className="title">Key</div>
-        <div>asdfaswer</div>
+export default class ApiKey extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      apiKey: ""
+    };
+  }
+
+  componentDidMount() {
+    this.getApiKey();
+  }
+
+  getApiKey() {
+    axios
+      .get("https://api.devcamp.space/get-client-applications", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          apiKey: response.data.client.auth_token
+        });
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <h2>API Key</h2>
+        <div className="two-column-grid">
+          <div className="title">Key</div>
+          <div>{this.state.apiKey}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
